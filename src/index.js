fd32007c558b4ae6333dcb474f654911415dd233
Sérgio Azevedo <sergio.azevedo@herzsache.com>
2022-11-14 16:55:45 +0000

    Initial test

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+/*
+    [] Find the command to start the local DB
+    to get all Users http://localhost:3000/users to get a specific user by ID http://localhost:3000/users/ID
+    [] Using Fetch / Axios / ... : 
+                                    [] Get all users 
+                                    [] Get user with ID 1  
+                                    [] With the list of all users find the one with the name "Delete Me" and delete it 
+    [] With the list of all users:
+                                    [] Have a variable with all the 'u_id'
+                                    [] Calculate the total of the users ages
+                                    [] Get the 2nd oldest person
+                                    [] Split the list by 3 Male, Female, Others
+                                    [] Print the total of users by birth date
+                                    [] Show the first user with the name "Laurent"
+                                    [] Sort the list by "u_id"
+*/
